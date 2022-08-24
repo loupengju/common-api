8ffbc5b71d13fbc670b34faf47c1e20ada3c1f27
pengju.lou <pengju.lou@weimob.com>
2022-08-24 16:28:42 +0800

    fix issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -19,28 +19,24 @@ export class AppController {
 
   @Get('/download')
   @Bind(Res({ passthrough: true }))
-  async downloadFile(@Query() query, @Res({ passthrough: true }) res: Response): Promise<StreamableFile> {
+  async downloadFile(@Query() query, @Res({ passthrough: true }) res: Response) {
     const { url } = query;
     const subFix = url.substring(url.lastIndexOf('.') + 1);
     const fingerPrint = url
       .split('/')
       .pop()
       ?.replace(`.${subFix}`, '');
-    const writer = createWriteStream(`./files/${fingerPrint}.${subFix}`);
     const response = await _axios.get(url, {responseType: 'stream'});
-    response.data.pipe(writer);
-    await new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
-    });
-
     res.set({
       'Content-Type': response.headers['content-type'],
       'Content-Disposition': `attachment; filename=${fingerPrint}.${subFix}`,
     });
 
-    const file = createReadStream(`./files/${fingerPrint}.${subFix}`);
-    const result = new StreamableFile(file);
-    return result;
+    response.data.pipe(res);
+
+    return new Promise((resolve, reject) => {
+      res.on('finish', resolve);
+      res.on('error', reject);
+   });
   }
 }
