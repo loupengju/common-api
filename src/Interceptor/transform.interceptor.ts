3e32121a3c8da474e68bce649ecd67f5f2cc3f64
pengju.lou <pengju.lou@weimob.com>
2022-09-05 09:59:52 +0800

    add excludes

--- src/Interceptor/transform.interceptor.ts
+++ src/Interceptor/transform.interceptor.ts
@@ -2,18 +2,21 @@ import { CallHandler, ExecutionContext, Injectable } from "@nestjs/common";
 import { execSync } from "child_process";
 import { map } from "rxjs/operators";
 
+const excludes = ['/download'];
+
 @Injectable()
 /**
  * @see https://nestjs.bootcss.com/interceptors#response-mapping
  *  */
 export class TransformInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler) {
+    const reqUrl = _context.switchToHttp().getRequest().url;
+    if (excludes.some(requestUrl => reqUrl.includes(requestUrl))) {
+      return next.handle();
+    };
+
     return next.handle().pipe(
       map((data) => {
-        const reqUrl = _context.switchToHttp().getRequest().url;
-        if (reqUrl.includes('/download')) {
-          return data;
-        };
         return {
           code: 0,
           message: "请求成功",
